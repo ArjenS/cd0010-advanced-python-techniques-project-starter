05eae2daef04aec934b4f62545b1f56ca1b6d6d0
Arjen <arjen.siegers@ns.nl>
2023-09-29 14:10:08 +0000

    write files

--- write.py
+++ write.py
@@ -25,10 +25,30 @@ def write_to_csv(results, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     fieldnames = (
-        'datetime_utc', 'distance_au', 'velocity_km_s',
-        'designation', 'name', 'diameter_km', 'potentially_hazardous'
+        "datetime_utc",
+        "distance_au",
+        "velocity_km_s",
+        "designation",
+        "name",
+        "diameter_km",
+        "potentially_hazardous",
     )
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
+    with open(filename, "w",newline='\n') as write_csv:
+        csv_writer = csv.writer(write_csv, delimiter=",")
+        csv_writer.writerow(fieldnames)
+        for line in results:
+            row = []
+            row.extend(
+                [
+                    line.time_str,
+                    line.velocity,
+                    line.neo.designation,
+                    line.neo.name,
+                    line.neo.diameter,
+                    line.neo.hazardous,
+                ]
+            )
+            csv_writer.writerow(row)
 
 
 def write_to_json(results, filename):
@@ -42,4 +62,18 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
+    output = []
+    for line in results:
+        line_dict = {}
+        line_dict["datetime_utc"] = line.time_str
+        line_dict["distance_au"] = line.distance
+        line_dict["velocity_km_s"] = line.velocity
+        line_dict["neo"] = {}
+        line_dict["neo"]["designation"] = line.neo.designation
+        line_dict["neo"]["name"] = line.neo.name
+        line_dict["neo"]["diameter_km"] = line.neo.diameter
+        line_dict["neo"]["potentially_hazardous"] = line.neo.hazardous
+        output.append(line_dict)
+
+    with open(filename, "w") as write_json:
+        json.dump(output, write_json)
